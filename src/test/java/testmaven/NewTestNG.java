905a27f7b944c3182200ffe88879759b67b60398
RajendraP3460 <rajendrap3460@gmail.com>
2021-03-22 17:47:20 -0400

    Modified 2 files from diff directory

--- src/test/java/testmaven/NewTestNG.java
+++ src/test/java/testmaven/NewTestNG.java
@@ -29,7 +29,8 @@ public class NewTestNG {
 	  
 	  System.out.println("launching Application");
 	  driver.get(baseURL);
-	  System.out.println(driver.get(baseURL););
+	  System.out.println(driver.get(baseURL));
+	  System.out.println("After Capturing the Title");
 	  String expectedTitle = "Welcome: Mercury Tours";
 	  String actualTitle = driver.getTitle();
 	  Assert.assertEquals(actualTitle, expectedTitle);
